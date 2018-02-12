62776831e3cac6f37498c635e66475508dff585a
VigneshVkl <35764577+VigneshVkl@users.noreply.github.com>
2018-02-12 16:08:14 +0530

    Create vicky1.c

---
+++ vicky1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main(void)  
+{
+	int a[10],temp;
+	clrscr();
+	int i;
+	for(i=1;i<10;i++)
+	{
+	 scanf(%d,7a[i]);
+	 }
+	  temp[0];
+	  for(i=1;i<10;i++)
+	  {
+	  	if(a[0]<a[i])
+	        {
+	        	temp a[i];
+	        	
+	        }
+	return 0;
+	  }
+}
+#include <stdio.h>
+int main(void)  
+{
+	int a[10],temp;
+	clrscr();
+	int i;
+	for(i=1;i<10;i++)
+	{
+	 scanf(%d,7a[i]);
+	 }
+	  temp[0];
+	  for(i=1;i<10;i++)
+	  {
+	  	if(a[0]<a[i])
+	        {
+	        	temp a[i];
+	        	
+	        }
+	return 0;
+	  }
+}
