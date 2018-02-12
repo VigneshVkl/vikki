856d3b1fd97beb9ea7a7f685b477ad45e2002779
VigneshVkl <35764577+VigneshVkl@users.noreply.github.com>
2018-02-12 15:52:31 +0530

    Create vicky.c

---
+++ vicky.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,i,a,b;
+clrscr();
+printf("enter the number of AP series\n");
+scanf("%d",&n);
+printf("enter first term & common diference");
+scanf("%d%d",&a,&b);
+value=a;
+for(i=1;i<=100000;i++)
+{
+printf("sum of AP series is %d is %d",a,b);
+return 0;
+}
+}
